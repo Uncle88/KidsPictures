e7ae68d76e7fed533ed229eda2dec728a23fc7f1
Uncle88 <andreyproskura88@gmail.com>
2017-10-31 17:14:57 +0200

    async/await sample

--- KidsPictures/KidsPictures/KidsPicturesPage.xaml.cs
+++ KidsPictures/KidsPictures/KidsPicturesPage.xaml.cs
@@ -1,12 +1,50 @@
-﻿using Xamarin.Forms;
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace KidsPictures
 {
     public partial class KidsPicturesPage : ContentPage
     {
+        private int _privateField;
+
         public KidsPicturesPage()
         {
             InitializeComponent();
+        }
+
+        private async void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            ////await method without await keyword
+            //var result = Method().GetAwaiter();
+            //result.OnCompleted(() =>
+            //{
+            //    var str = "we finishe async work";
+            //    _label.Text = str;
+            //});
+
+            await Method();
+            var str = "we finishe async work";
+            _label.Text = str;
+            }
+
+        protected virtual async Task Method()
+        {
+            _privateField = 10;
+            await Task.Run(async () =>
+            {
+                _label.Text = "her tobi";
+                await Task.Delay(3000);
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    _label.Text = "jooo";
+                });
+                await Task.Delay(2000);
+            });
+        }
+
+        private int NotAsyncMethod()
+        {
+            return 1;
         }
     }
 }
